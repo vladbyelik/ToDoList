d6f598457ed80891d61c13f2729e737006afd93d
vladbyelik <vladbyelik@gmail.com>
2021-03-25 14:37:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { v4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = ({ toDoLength }) => <header>You have { toDoLength } todos</header>;
+
+class SubmitForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange(e) {
+    const value = e.target.value;
+    this.setState({ value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const value = this.state.value;
+    if(value) {
+      this.props.onAdd(value);
+      this.setState({ value: '' });
+    }
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <input type="submit" value="Add" />
+      </form>
+    )
+  }
+}
+
+const ToDoList = ({ list, onDelete }) => {
+  return(
+    <ul>
+      {list.map(element => {
+        const {id, value} = element;
+        return (
+          <li key={id}>
+            {value}
+            <button onClick={() => onDelete(id)}>Delete</button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+class App extends React.Component {
+
+  state = {
+    list: []
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleAdd(value) {
+    this.setState({
+      list: this.state.list.concat({
+        id: v4(),
+        value
+      })
+    })
+  }
+
+  handleDelete(id) {
+    this.setState(prev => {
+      return {
+        list: prev.list.filter(element =>  element.id !== id)
+      }
+    })
+  }
+
+  render() {
+    const length = this.state.list.length;
+    const list = this.state.list;
+    return (
+      <div className="App">
+        <Header toDoLength={length}/>
+        <SubmitForm onAdd={this.handleAdd}/>
+        <ToDoList onDelete={this.handleDelete} list={list}/>
+      </div>
+    );
+  }
 }
 
 export default App;
